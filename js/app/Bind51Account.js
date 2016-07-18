ae6610139f3ea18a658887f33e143d9ecef0caf8
springxiege <springxiege@sina.cn>
2016-07-18 18:23:49 +0800

    clean

--- js/app/Bind51Account.js
+++
@@ -1,8 +0,0 @@
-import React,{Component} from 'react';
-export default class Bind51Account extends Component{
-    render(){
-        return (
-            
-        )
-    }
-}
--- js/components/ReturnTop.js
+++
@@ -1,8 +0,0 @@
-import React,{Component} from 'react';
-export default class ReturnTop extends Component{
-    render(){
-        return (
-            <div className="returntop"></div>
-        )
-    }
-}
